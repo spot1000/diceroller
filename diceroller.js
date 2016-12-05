309b754532ded44d423a4d571394ba16de0f0a05
Adam Lichter <adamlichter@gmail.com>
2016-12-05 10:34:42 -0330

    Working dicerolling algorithm

--- diceroller.js
+++ diceroller.js
@@ -1,46 +1,20 @@
-
-var target = 7;
-var numOfDice = 2;
-var newResults = [];
-var sides = 6;
-
-/*
-for (var w = 0; w <= sides; w++) {
-  singleArr.push(1)
-}
-
-for (var z = 0; z < 12; z++){
-  results.push(0);
-}
-
-
-for (var x = 0; x < sides; x++) {
-  for (var q = 1; q <= sides; q++) {
-    results[x+q] += singleArr[q];
-  }
-}
-
-console.log(results)
-*/
-
-
-
-function diceRoller(target, numOfDice, sides) {
-  var results = [];
-  for (var x = 0; x <= target; x++) {
-    results.push(0)
-  }
-  newResults = results.slice();
-  results[0] = 1;
-
-  for (var q = 0; q < numOfDice; q++) {
-    for (var w = 1; w <= sides && q+w < target; w++) {
-      results[q+w] = results[q];
+function diceRoller(target, numOfDice, sides, count = 0) {
+  var total = 0;
+  if (numOfDice == 1) {
+    for (var x = 1; x <= sides; x++) {
+      if (x + count <= target){
+        total++;
+      }
     }
-    results[q] = 0;
+    return total;
   }
-  return results;
-
+  else {
+    for (var q = 1; q <= sides; q++) {
+      total += diceRoller(target, numOfDice-1, sides, q+count);
+    }
+    return total;
+  }
 }
 
-console.log(diceRoller(7,2,6));
+console.log(diceRoller(7, 2, 6));
+console.log(diceRoller(11, 3, 6));
