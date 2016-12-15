41d791c6452777405779429afa10b5d6f6750884
Adam Lichter <adamlichter@gmail.com>
2016-12-15 23:22:03 +0000

    slider works

--- ui.js
+++ ui.js
@@ -1,32 +1,20 @@
 var dice = document.getElementById("dice"),
-    diceMinus = document.getElementById("dice-"),
-    dicePlus = document.getElementById("dice+"),
     sides = document.getElementById("sides"),
-    sidesMinus = document.getElementById("sides-"),
-    sidesPlus = document.getElementById("sides+"),
-    thresh = document.getElementById("thresh"),
-    threshMinus = document.getElementById("thresh-"),
-    threshPlus = document.getElementById("thresh+");
+    thresh = document.getElementById("thresh");
     
-function assignOnclick(minus, plus, target) {
-    minus.onclick = function() {
-       target.innerHTML--;
-       calculate();
-    };
-    plus.onclick = function() {
-        target.innerHTML++;
-        calculate();
-    };
-}
+function assignOnclick(element) {
+   //element.onchange = calculate;
+   } 
 
-assignOnclick(diceMinus, dicePlus, dice);
-assignOnclick(sidesMinus, sidesPlus, sides);
-assignOnclick(threshMinus, threshPlus, thresh);
+
+assignOnclick(dice);
+assignOnclick(sides);
+assignOnclick(thresh);
 
 function calculate() {
   document.getElementById("chance").innerHTML = percentage(
-    parseInt(thresh.innerHTML), parseInt(dice.innerHTML),
-    parseInt(sides.innerHTML), false); 
+    parseInt(thresh.value), parseInt(dice.value),
+    parseInt(sides.value), false); 
 }
 
 calculate();
